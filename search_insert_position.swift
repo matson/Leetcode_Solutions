93f1f9f799e9002f21a67c3a0d6a94789e8b71cd
Matson <madison.k.adams@gmail.com>
2025-07-03 10:25:58 -0400

    Create search_insert_position.swift

---
+++ search_insert_position.swift
@@ -0,0 +1,28 @@
+func searchInsert(nums: [Int], target: Int) -> Int {
+    // binary search:
+    var n: Int = nums.count
+    //left
+    var left: Int = 0 // first index
+    //right
+    var right = n - 1
+    // if the target is present
+    while left <= right{
+        var middle = left + (right - left) / 2
+        if nums[middle] == target{
+            return middle
+        }
+        else if nums[middle] < target{
+            left = middle + 1
+        }else{
+            right = middle - 1
+            
+        }
+    }
+    //else return the left
+    return left
+    
+}
+
+// test cases
+print(searchInsert(nums: [1,3,5,6], target: 5)) // should be 2
+print(searchInsert(nums: [1,3,5,6], target: 2)) // should be 1
