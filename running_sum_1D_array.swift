7b0a0ccca142ad7432f6602179e326118e14f398
Matson <madison.k.adams@gmail.com>
2025-07-03 19:57:55 -0400

    Update running_sum_1D_array.swift

--- running_sum_1D_array.swift
+++ running_sum_1D_array.swift
@@ -1,5 +1,6 @@
 // Category: prefix sum
 
+// king way: O(n)
 func running_Sum(nums: [Int]) -> [Int] {
     
     // instead of recomputing the sum from scratch for each index, you keep a running total
@@ -14,6 +15,25 @@ func running_Sum(nums: [Int]) -> [Int] {
     }
     
     return arr_t
+}
+
+print(running_Sum(nums: [1,2,3,4])) // should be [1,3,6,10]
+print(running_Sum(nums: [1,1,1,1,1])) // should be [1,2,3,4,5]
+
+// brute force: O(n^2)
+func running_Sum(nums: [Int]) -> [Int]{
+   // brute force way
+   var arr_t: [Int] = []
+   
+   for i in 0..<nums.count {
+       var total: Int = 0
+       // reduce function
+       for j in 0...i{
+           total += nums[j]
+       }
+       arr_t.append(total)
+   }
+   return arr_t
 }
 
 print(running_Sum(nums: [1,2,3,4])) // should be [1,3,6,10]
