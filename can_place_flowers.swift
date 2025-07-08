78fdbb1b74599f76b7f1e4f3c3df5fbf9cd46d37
Matson <madison.k.adams@gmail.com>
2025-07-08 16:19:29 -0400

    Create can_place_flowers.swift

---
+++ can_place_flowers.swift
@@ -0,0 +1,30 @@
+
+// Category: greedy 
+
+func canPlaceFlowers(flowerbed: [Int], n: Int) -> Bool {
+    
+    var bed = flowerbed
+    var count = n
+    
+    // brute force
+    for i in 0..<bed.count {
+        if bed[i] == 0{
+            let emptyLeft = ( i == 0 || bed[i-1] == 0)
+            let emptyRight = (i == bed.count - 1 || bed[i+1] == 0)
+            
+            if emptyLeft && emptyRight {
+                bed[i] = 1
+                // decrement count
+                count -= 1
+                if count == 0 {
+                    return true
+                }
+            }
+        }
+    }
+    // if the count is 0 then it will be true 
+    return count <= 0
+}
+
+print(canPlaceFlowers(flowerbed: [1,0,0,0,1], n: 1)) // true
+print(canPlaceFlowers(flowerbed: [1,0,0,0,1], n: 2)) // false
