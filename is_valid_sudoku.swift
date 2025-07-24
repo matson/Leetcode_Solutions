c6383cca9518be4138386f0c83e3b5d4f860ac64
Matson <madison.k.adams@gmail.com>
2025-07-23 21:23:35 -0400

    Update is_valid_sudoku.swift

--- is_valid_sudoku.swift
+++ is_valid_sudoku.swift
@@ -76,3 +76,31 @@ print(isValidSudoku(board: [["1","2",".",".","3",".",".",".","."],
                             [".",".",".",".",".",".","2",".","."],
                             [".",".",".","4","1","9",".",".","8"],
                             [".",".",".",".","8",".",".","7","9"]])) // false
+
+// O(9^2) 
+// with hash set 
+func isValidSudoku(_ board: [[Character]]) -> Bool {
+        var cols = [Int: Set<Character>]()
+        var rows = [Int: Set<Character>]()
+        var squares = [String: Set<Character>]()
+
+        for r in 0..<9 {
+            for c in 0..<9 {
+                if board[r][c] == "." { continue }
+
+                let squareKey = "\(r / 3),\(c / 3)"
+
+                if rows[r]?.contains(board[r][c]) == true ||
+                   cols[c]?.contains(board[r][c]) == true ||
+                   squares[squareKey]?.contains(board[r][c]) == true {
+                    return false
+                }
+
+                rows[r, default: []].insert(board[r][c])
+                cols[c, default: []].insert(board[r][c])
+                squares[squareKey, default: []].insert(board[r][c])
+            }
+        }
+        
+        return true
+}
