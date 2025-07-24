30fbff05d2ce0fed9cf2919ce74669d5f1a056d8
Matson <madison.k.adams@gmail.com>
2025-07-24 10:31:44 -0400

    Update longest_consecutive.swift

--- longest_consecutive.swift
+++ longest_consecutive.swift
@@ -30,3 +30,31 @@ func longestConsecutive(nums: [Int]) -> Int {
 print(longestConsecutive(nums: [2,20,4,10,3,4,5])) // 4
 print(longestConsecutive(nums: [0,3,2,5,4,6,1,1])) // 7
 print(longestConsecutive(nums: [9,1,4,7,3,-1,0,5,8,-1,6])) // 7
+
+// O(n)
+func longestConsecutive(nums: [Int]) -> Int {
+   
+    // use a set
+    // check if each have neighbors
+    var set = Set(nums)
+    var longest: Int = 0
+    
+    for i in 0..<nums.count{
+        // check if it's the start of a sequence
+        if !set.contains(nums[i]-1) {
+            var length = 1 // add 1 to the start
+            // while the additions exist
+            while set.contains(nums[i] + length){
+                // keep adding to find rest of numbers in sequence
+                length += 1
+            }
+            longest = max(length, longest)
+        }
+    }
+    return longest
+}
+
+// [1, 100, 200, 2, 3] -> check left, check i + n, update 
+print(longestConsecutive(nums: [2,20,4,10,3,4,5])) // 4
+print(longestConsecutive(nums: [0,3,2,5,4,6,1,1])) // 7
+print(longestConsecutive(nums: [9,1,4,7,3,-1,0,5,8,-1,6])) // 7
