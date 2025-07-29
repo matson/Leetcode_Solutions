c61cb3fc9c5e6850904753797b0dd23b7ccd3f18
Matson <madison.k.adams@gmail.com>
2025-07-29 10:53:45 -0400

    Update contains_duplicate_2.swift

--- contains_duplicate_2.swift
+++ contains_duplicate_2.swift
@@ -1,6 +1,49 @@
 
+// O(n)
+// Category: sliding window
+// Level: easy 
+func containsDuplicate(nums: [Int], k: Int) -> Bool {
+    // optimized way
+    // sliding window
+    var window = Set<Int>()
+    var left: Int = 0
+    
+    for r in 0..<nums.count {
+        if (r - left) > k {
+            window.remove(nums[left])
+            left += 1
+        }
+        if window.contains(nums[r]){
+            return true
+        }
+        window.insert(nums[r])
+    }
+    return false
+}
+
+// "at most k elements"
+func isWindow(nums: [Int], k: Int) -> Bool {
+    
+    var window = Set<Int>() // make a set
+    var left: Int = 0
+    
+    for r in 0..<nums.count {
+        if r - left + 1 > k { // if it exceeds window size
+            window.remove(nums[left])
+            left += 1 // move left up
+        }
+        if window.contains(nums[r]){
+            return true
+        }
+        window.insert(nums[r]) // insert in the set
+    }
+    return false
+    
+}
+
+
 // O(n^2)
-func containsDuplicate2(nums: [Int], k: Int) -> Bool {
+func containsDuplicate(nums: [Int], k: Int) -> Bool {
     
     // brute force way
     // awful I know
@@ -20,7 +63,7 @@ print(containsDuplicate2(nums: [1,2,3,1], k: 3)) // true
 print(containsDuplicate2(nums: [1,0,1,1], k: 1)) // true
 print(containsDuplicate2(nums: [1,2,3,1,2,3], k: 2)) // false
 
-func containsDuplicate2(nums: [Int], k: Int) -> Bool {
+func containsDuplicate(nums: [Int], k: Int) -> Bool {
     
     // last seen
     var lastSeen: [Int: Int] = [:]
