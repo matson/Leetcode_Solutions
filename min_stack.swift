e07b708fe1f77847f7dfcbda5d4db37566666c80
Matson <madison.k.adams@gmail.com>
2025-08-03 18:37:36 -0400

    Update min_stack.swift

--- min_stack.swift
+++ min_stack.swift
@@ -1,4 +1,39 @@
 
+// Category: stacks
+// Level: Medium
+
+// O(1)
+// using two stacks
+class MinStack {
+    
+    private var stack: [Int] = []
+    private var minStack: [Int] = []
+    
+    init(){}
+    
+    func push(val: Int){
+        stack.append(val)
+        // compare input with the last value added to stack 
+        let minVal = min(val, minStack.last ?? val)
+        minStack.append(minVal)
+    }
+    
+    func pop(){
+        stack.popLast()
+        minStack.popLast()
+    }
+    
+    func top() -> Int {
+        return stack.last!
+    }
+    
+    func getMin() -> Int{
+        return minStack.last!
+    }
+}
+
+
+
 
 // brute force
 class MinStack {
