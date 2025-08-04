34b4ba4c05dd17b13813cedb658b9a421328a483
Matson <madison.k.adams@gmail.com>
2025-08-04 19:54:34 -0400

    Update guess_number.swift

--- guess_number.swift
+++ guess_number.swift
@@ -8,6 +8,16 @@
 // O(n) -> O(log n)
 // instead of array, binary search on range of values
 
+
+
+
+
+
+// * Binary Search Variant *
+
+// will be sorted order
+// O(n) -> O(log n)
+// instead of array, binary search on range of values
 func guessNumber(n: Int) -> Int {
     
     var low: Int = 1
@@ -16,13 +26,20 @@ func guessNumber(n: Int) -> Int {
     while low <= high {
         var middle = (low + high) / 2
         var res = guess(middle)
+        // 1, guess is lower
         if (res > 0) {
             low = middle + 1
+        // -1, guess is higher
         }else if (res < 0) {
             high = middle - 1
         }else{
+            // 0, equal to
             return middle
         }
     }
     return -1
 }
+
+
+
+
