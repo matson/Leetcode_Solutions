dfbcdd60cf75f52cb9ecf3beb30f2043f9044729
Matson <madison.k.adams@gmail.com>
2025-08-07 08:26:44 -0400

    Update search_insert_position.swift

--- search_insert_position.swift
+++ search_insert_position.swift
@@ -26,6 +26,28 @@ func searchInsert(nums: [Int], target: Int) -> Int {
     
 }
 
+ // O(nlogn)
+func searchInsertPosition(nums: [Int], target: Int) -> Int {
+    var arr = nums
+    // brute force
+    
+    for i in 0..<nums.count{
+        if target == nums[i]{
+            return i
+        }
+    }
+    
+    // if not already present
+    arr.append(target)
+    arr.sort()
+    for j in 0..<arr.count{
+        if target == arr[j]{
+            return j
+        }
+    }
+    return -1
+}
+
 // test cases
 print(searchInsert(nums: [1,3,5,6], target: 5)) // should be 2
 print(searchInsert(nums: [1,3,5,6], target: 2)) // should be 1
