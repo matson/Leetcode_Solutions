2581955434996d25278cdbb856a9bfac25f086ea
Matson <madison.k.adams@gmail.com>
2025-08-15 16:32:47 -0400

    Create search_bst.swift

---
+++ search_bst.swift
@@ -0,0 +1,41 @@
+
+
+// category: trees
+// Level: easy
+// O(logn)
+func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+    // run binary search on the tree
+    guard let root = root else{
+        return nil
+    }
+    
+    // at root
+    if val > root.value{
+        // search to the right
+        searchBST(root.right, val: value)
+        
+    }else if val < root.value{
+        // search to the left
+        searchBST(root.left, val: value)
+    } else {
+        // we found that node
+        return root
+    }
+    
+}
+
+func searchTree(root: TreeNode?, target: Int) -> Bool{
+    guard root != nil else {return false}
+    
+    if target > root?.value{
+        // go right
+        return searchTree(root: root?.right, target: target)
+    }
+    else if target < root?.value{
+        // go left
+        return searchTree(root: root?.left, target: target)
+    }else{
+        // return the node 
+        return true
+    }
+}
