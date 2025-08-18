1857f385c643fbf5288395a9af8cb9e3eb3a1031
Matson <madison.k.adams@gmail.com>
2025-08-18 18:29:11 -0400

    Update binary_tree_inOrder.swift

--- binary_tree_inOrder.swift
+++ binary_tree_inOrder.swift
@@ -20,3 +20,35 @@ func inOrderTrav(root: TreeNode?) -> [Int]{
     inOrder(root: root)
     return arr
 }
+
+// Depth First Search
+// In Order traversal
+// O(n)
+func inOrder(root: TreeNode?){
+    guard let root = root else{
+        return
+    }
+    
+    inOrder(root: root.left)
+    print(root.val)
+    inOrder(root: root.right)
+}
+
+func postOrder(root: TreeNode?){
+    guard let root = root else{
+        return
+    }
+    postOrder(root: root.left)
+    postOrder(root: root.right)
+    print(root.val)
+}
+
+func preOrder(root: TreeNode?){
+    guard let root = root else{
+        return
+    }
+    print(root.val)
+    preOrder(root: root.left)
+    preOrder(root: root.right)
+    
+}
