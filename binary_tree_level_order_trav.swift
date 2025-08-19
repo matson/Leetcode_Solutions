149481a1e64d8f8b748224eb03961a0e53845f18
Matson <madison.k.adams@gmail.com>
2025-08-19 16:44:20 -0400

    Update binary_tree_level_order_trav.swift

--- binary_tree_level_order_trav.swift
+++ binary_tree_level_order_trav.swift
@@ -26,3 +26,32 @@ func levelOrderTrav(root: TreeNode?) -> [[Int]]{
     }
     return result
 }
+
+// original algorithm
+func levelOrder(root: TreeNode?){
+    // using a queue
+    var queue: [TreeNode?] = []
+    
+    if root != nil{
+        queue.append(root)
+    }
+    
+    var level = 0
+    while queue.count > 0{
+        // nodes in queue
+        print("level", level)
+        for i in 0...queue.count - 1{
+            // first in first out
+            let current = queue.removeFirst()
+            // does it have right or left children
+            print(current?.val)
+            if current?.left != nil{
+                queue.append(current?.left)
+            }
+            if current?.right != nil {
+                queue.append(current?.right)
+            }
+        }
+        level += 1
+    }
+}
