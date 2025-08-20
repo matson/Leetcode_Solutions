310e213900aaf8e89c8dae077d16c64a9402aac0
Matson <madison.k.adams@gmail.com>
2025-08-20 14:47:54 -0400

    Create invert_tree.swift

---
+++ invert_tree.swift
@@ -0,0 +1,18 @@
+// category: trees
+// level: easy
+// O(n)
+
+func invertTree(root: TreeNode?)-> TreeNode?{
+    // base case 
+    guard let root = root else{
+        return nil
+    }
+    // switch the pointers
+    // need to do this right or else it will screw up the pointers
+    (root.left, root.right) = (root.right, root.left)
+    // inOrder trav. 
+    invertTree(root: root.left)
+    invertTree(root: root.right)
+  
+    return root
+}
